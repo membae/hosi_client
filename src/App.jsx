a962aa75136f621891ae0074593fd27f67731d05
membae <membaphilip1@gmail.com>
2026-01-06 02:00:27 +0300

    app route

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,12 @@
 import React from 'react'
+import {Route,Routes} from 'react-router-dom'
+import Signup  from './pages/Signup'
 
 function App() {
   return (
-    <div>App</div>
+    <Routes>
+      <Route path='/signup' element={<Signup/>}></Route>
+    </Routes>
   )
 }
 
