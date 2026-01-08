75705e995c74faf14087e97936d08448f32d08b8
membae <membaphilip1@gmail.com>
2026-01-08 22:36:20 +0300

    manage users route

--- src/App.jsx
+++ src/App.jsx
@@ -2,12 +2,14 @@ import React from 'react'
 import {Route,Routes} from 'react-router-dom'
 import Signup  from './pages/Signup'
 import Login from './pages/Login'
+import ManageUsers from './components/admin/ManageUsers'
 
 function App() {
   return (
     <Routes>
       <Route path='/signup' element={<Signup/>}></Route>
       <Route path='/login' element={<Login/>}></Route>
+      <Route path='/manageUsers' element={<ManageUsers/>}></Route>
     </Routes>
   )
 }
